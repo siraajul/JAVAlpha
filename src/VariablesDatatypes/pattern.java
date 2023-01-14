ecb4b71ac31d8ce980da6b5661bfc5603f895746
itsirajul <shirajulislamparvez@gmail.com>
2023-01-15 05:32:27 +0600

    Java Variables

---
+++ src/VariablesDatatypes/pattern.java
@@ -0,0 +1,10 @@
+package VariablesDatatypes;
+
+public class pattern {
+    public static void main(String[] args) {
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
---
+++ src/VariablesDatatypes/variables.java
@@ -0,0 +1,16 @@
+package VariablesDatatypes;
+
+public class variables {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 5;
+       System.out.println(a);
+       System.out.println(b);
+       String name="Tony Stark";
+       System.out.println(name);
+
+       a=50;
+        System.out.println(a);
+
+    }
+}
