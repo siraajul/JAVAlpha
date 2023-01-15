3ec7c540f3b85e384ac32225010684144309b3df
itsirajul <shirajulislamparvez@gmail.com>
2023-01-15 06:51:50 +0600

    Java Input Problems

---
+++ src/VariablesDatatypes/JavaCommnets.java
@@ -0,0 +1,14 @@
+package VariablesDatatypes;
+
+public class JavaCommnets {
+    public static void main(String[] args) {
+
+        // Single Line Comment
+
+
+        /*
+        Multiline Comments
+         */
+
+    }
+}
---
+++ src/VariablesDatatypes/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package VariablesDatatypes;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+
+        int first_number = 30;
+        int second_number = 40;
+
+        int sum = first_number+second_number;
+        System.out.println(sum);
+    }
+}
