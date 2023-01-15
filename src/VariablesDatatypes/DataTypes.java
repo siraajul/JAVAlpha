e8f6a12f14e90900d37c2a24bc84059d4e78a3c0
itsirajul <shirajulislamparvez@gmail.com>
2023-01-15 06:07:54 +0600

    Java DataTypes Check

---
+++ src/VariablesDatatypes/DataTypes.java
@@ -0,0 +1,22 @@
+package VariablesDatatypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte b=8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+        //boolean var = false;
+
+        float price = 10.5F;
+        System.out.println(price);
+
+        int number =25;
+        System.out.println(number);
+
+        short n= 240;
+        System.out.println(n);
+    }
+}
