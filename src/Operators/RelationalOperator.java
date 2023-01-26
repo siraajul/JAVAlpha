302978e9468efc5bc4437864f9dcd18c232721e1
itsirajul <shirajulislamparvez@gmail.com>
2023-01-26 20:25:35 +0600

    Relational Operator

--- src/Operators/RelationalOperator.java
+++ src/Operators/RelationalOperator.java
@@ -1,4 +1,20 @@
 package Operators;
 
+import java.util.Scanner;
+
 public class RelationalOperator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Check Tow Number Equal or Not,\nIf Equal will Return True If Not Equal Will Return False");
+        System.out.println("-----------------------------");
+        System.out.print("Enter First Number: ");
+        int A = input.nextInt();
+        System.out.print("Enter Second Number: ");
+        int B = input.nextInt();
+        System.out.println((A >= B));
+
+
+    }
 }
--- src/Operators/UnaryOperator.java
+++ src/Operators/UnaryOperator.java
@@ -1,4 +1,16 @@
 package Operators;
 
+import java.util.Scanner;
+
 public class UnaryOperator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = --a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
