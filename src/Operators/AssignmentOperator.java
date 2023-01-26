dfea503946b271a5a23a4e94c021b81bfbdeee85
itsirajul <shirajulislamparvez@gmail.com>
2023-01-26 21:20:53 +0600

    Assignment Operator

--- src/Operators/AssignmentOperator.java
+++ src/Operators/AssignmentOperator.java
@@ -1,4 +1,13 @@
 package Operators;
 
 public class AssignmentOperator {
+    public static void main(String[] args) {
+        int A = 10;
+        A+=10;
+        System.out.println(A);
+
+        int B = 5;
+        B-=5;
+        System.out.println(B);
+    }
 }
