003d13e21bd5cd82f50cdfc9d8ff205ffb91ece3
itsirajul <shirajulislamparvez@gmail.com>
2023-01-26 23:21:38 +0600

    Assignments

--- src/Operators/Assignment/ProblemOne.java
+++ src/Operators/Assignment/ProblemOne.java
@@ -1,4 +1,14 @@
 package Operators.Assignment;
 
 public class ProblemOne {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 5;
+
+        int exp1 = (x * y / x);
+        int exp2 = (x* (y / x));
+
+        System.out.print(exp1+",");
+        System.out.print(exp2);
+    }
 }
--- src/Operators/Assignment/ProblemTwo.java
+++ src/Operators/Assignment/ProblemTwo.java
@@ -1,4 +1,19 @@
 package Operators.Assignment;
 
 public class ProblemTwo {
+    public static void main(String[] args) {
+        int x=200;
+        int y=50;
+        int z=100;
+
+        if(x>y && y>z){
+            System.out.println("Hello");
+        }
+        if(z>y && z<x){
+            System.out.println("Java");
+        }
+        if((y+200)<x && (y+150)<z){
+            System.out.println("Hello Java");
+        }
+    }
 }
