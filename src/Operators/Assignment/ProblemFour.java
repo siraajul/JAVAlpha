9607398e9182c6a2489c53fdc0cf38c8cabc1cd3
itsirajul <shirajulislamparvez@gmail.com>
2023-01-26 23:29:54 +0600

    Assignments

--- src/Operators/Assignment/ProblemFour.java
+++ src/Operators/Assignment/ProblemFour.java
@@ -1,4 +1,14 @@
 package Operators.Assignment;
 
 public class ProblemFour {
+    public static void main(String[] args) {
+        int x= 9;
+        int y= 12;
+        int a=2;
+        int b=4;
+        int c=6;
+
+        int exp = 4/3*(x+34)+9*(a+b*c)+(3+y*(2+a))/(a+b*y);
+        System.out.println(exp);
+    }
 }
