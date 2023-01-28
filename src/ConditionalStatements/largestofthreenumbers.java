38846d33b301205a96becb517740370717405cf0
itsirajul <shirajulislamparvez@gmail.com>
2023-01-28 23:55:21 +0600

    Conditional Statements

--- src/ConditionalStatements/largestofthreenumbers.java
+++ src/ConditionalStatements/largestofthreenumbers.java
@@ -1,4 +1,12 @@
 package ConditionalStatements;
 
+import java.util.Scanner;
+
 public class largestofthreenumbers {
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+
+
+    }
 }
--- src/ConditionalStatements/largesttwonumber.java
+++ src/ConditionalStatements/largesttwonumber.java
@@ -1,4 +1,22 @@
 package ConditionalStatements;
 
+import java.util.Scanner;
+
 public class largesttwonumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        if(A >= B){
+            System.out.println("A is Largest of 2");
+        }
+
+        else{
+            System.out.println("B is Largest of 2");
+        }
+    }
+
+
 }
