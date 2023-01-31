f17a7f224d73fd8fb78243f452d0eb83fe88fea0
itsirajul <shirajulislamparvez@gmail.com>
2023-02-01 02:57:44 +0600

    Leap Year

--- src/ConditionalStatements/Assignments/QuestionFive.java
+++ src/ConditionalStatements/Assignments/QuestionFive.java
@@ -1,4 +1,24 @@
 package ConditionalStatements.Assignments;
 
+import java.util.Scanner;
+
 public class QuestionFive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input Year");
+        int year = input.nextInt();
+
+        boolean x = (year%4) == 0;
+        boolean y = (year%100)!= 0;
+        boolean z = (year%400) ==0;
+
+        if(x &&(y||z)){
+            System.out.println(year+" is a Leap year");
+        }
+        else
+        {
+            System.out.println(year+" is not a leap year");
+        }
+    }
 }
--- src/ConditionalStatements/Assignments/QuestionFour.java
+++ src/ConditionalStatements/Assignments/QuestionFour.java
@@ -5,8 +5,8 @@ public class QuestionFour {
         int a=63;
         int b=36;
 
-        boolean x =(a<b)?true:false;
-        int y=(a>b)?a:b;
+        boolean x = (a < b)?true:false;
+        int y = (a > b) ? a : b;
         System.out.println(x);
         System.out.println(y);
     }
