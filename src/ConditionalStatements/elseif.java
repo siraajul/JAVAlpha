fa52adee47bbdafdb7a701f6bcd9c2562e097d67
itsirajul <shirajulislamparvez@gmail.com>
2023-01-31 21:28:05 +0600

    Conditional Statesment

--- src/ConditionalStatements/elseif.java
+++ src/ConditionalStatements/elseif.java
@@ -1,7 +1,24 @@
 package ConditionalStatements;
 
+import java.util.Scanner;
+
 public class elseif {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Your Age");
+        int age = input.nextInt();
+
+        if (age>=18){
+            System.out.println("Adult");
+        }
+        else if(age>=13 && age<18){
+            System.out.println("TeenAger");
+        }
+        else {
+            System.out.println("Child");
+        }
 
     }
 }
