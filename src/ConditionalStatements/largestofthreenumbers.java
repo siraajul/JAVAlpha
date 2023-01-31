48fa7e4a5f0ef421c71e47d2fb5992cc973251b5
itsirajul <shirajulislamparvez@gmail.com>
2023-01-31 21:43:50 +0600

    Check Largest of Theee Number

--- src/ConditionalStatements/largestofthreenumbers.java
+++ src/ConditionalStatements/largestofthreenumbers.java
@@ -7,6 +7,22 @@ public class largestofthreenumbers {
 
         Scanner input =new Scanner(System.in);
 
+        System.out.print("Enter First Number : ");
+        int a= input.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b= input.nextInt();
+        System.out.print("Enter Third Number : ");
+        int c= input.nextInt();
+
+        if((a>=b) && (a>=c)){
+            System.out.println(a+" is the largest.");
+        } else if (b>=c) {
+            System.out.println(b+" is the largest.");
+        }
+        else{
+            System.out.println(c+" is the largest.");
+        }
+
 
     }
 }
