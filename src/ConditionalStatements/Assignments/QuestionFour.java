9358814aa91457844ea1915304449c9cbd291696
itsirajul <shirajulislamparvez@gmail.com>
2023-02-01 02:31:13 +0600

    Ternary Operator

--- src/ConditionalStatements/Assignments/QuestionFour.java
+++ src/ConditionalStatements/Assignments/QuestionFour.java
@@ -1,4 +1,13 @@
 package ConditionalStatements.Assignments;
 
 public class QuestionFour {
+    public static void main(String[] args) {
+        int a=63;
+        int b=36;
+
+        boolean x =(a<b)?true:false;
+        int y=(a>b)?a:b;
+        System.out.println(x);
+        System.out.println(y);
+    }
 }
