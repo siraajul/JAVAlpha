692f0d3cbc827214712e7f810512498dce056dc3
itsirajul <shirajulislamparvez@gmail.com>
2023-02-01 02:10:17 +0600

    Conditional Assignment Question

--- src/ConditionalStatements/Assignments/QuestionTwo.java
+++ src/ConditionalStatements/Assignments/QuestionTwo.java
@@ -1,4 +1,16 @@
 package ConditionalStatements.Assignments;
 
+import java.util.Scanner;
+
 public class QuestionTwo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double temp = input.nextDouble();
+
+        if(temp>=100){
+            System.out.println("You Have Fever");
+        }
+    }
 }
