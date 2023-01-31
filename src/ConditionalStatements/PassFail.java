fa5c6f248a5b17159813734f4586f0558d8c745c
itsirajul <shirajulislamparvez@gmail.com>
2023-01-31 23:10:47 +0600

    Switch Statements

--- src/ConditionalStatements/PassFail.java
+++ src/ConditionalStatements/PassFail.java
@@ -1,4 +1,18 @@
 package ConditionalStatements;
 
+import java.util.Scanner;
+
 public class PassFail {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Students Marks");
+        int marks= input.nextInt();
+
+        String status =(marks>=33) ? "Pass":"Fail";
+        System.out.println(status);
+
+
+    }
 }
