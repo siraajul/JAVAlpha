7097e6e615923d3af5b3bf435f31ba9bc99a6da4
itsirajul <shirajulislamparvez@gmail.com>
2023-02-02 17:30:52 +0600

    Print N Numbers(User Input)

--- src/Loops/oneton.java
+++ src/Loops/oneton.java
@@ -1,4 +1,20 @@
 package Loops;
 
+
+import java.util.Scanner;
+
 public class oneton {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Range of Number: ");
+        int range = input.nextInt();
+        int counter =1;
+        while (counter<=range){
+            System.out.print(counter+" ");
+            counter++;
+        }
+    }
+
 }
