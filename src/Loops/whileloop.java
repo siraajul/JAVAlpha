bc65c35f70e53f5f0552ef055459481aa0120414
itsirajul <shirajulislamparvez@gmail.com>
2023-02-02 17:06:29 +0600

    While Loop

--- src/Loops/whileloop.java
+++ src/Loops/whileloop.java
@@ -1,4 +1,17 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class whileloop {
+    public static void main(String[] args) {
+
+        //Scanner input = new Scanner(System.in);
+        int counter = 0;
+
+        while(counter<100){
+            System.out.println("Hello Java");
+            counter++;
+        }
+        System.out.println("Printed 100x Times");
+    }
 }
