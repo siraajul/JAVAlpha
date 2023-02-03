b77c7d122b71acb59f51240d5165113afbeed7e4
itsirajul <shirajulislamparvez@gmail.com>
2023-02-03 19:13:36 +0600

    Loop Revisit

--- src/Loops/Basics.java
+++ src/Loops/Basics.java
@@ -3,6 +3,12 @@ package Loops;
 public class Basics {
     public static void main(String[] args) {
 
+        int counter = 0;
+        while (counter<=100){
+            System.out.print(counter+" ");
+            counter++;
+        }
+
         //While Loop
         //For Loop
         //do While Loop
--- src/Loops/firstNnaturalNumbers.java
+++ src/Loops/firstNnaturalNumbers.java
@@ -1,4 +1,21 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class firstNnaturalNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Number: ");
+        int n = input.nextInt();
+        int sum = 0;
+        int counter=1;
+        while (counter<=n){
+            sum+=counter;
+            counter++;
+        }
+        System.out.println("Sum of "+n+" is "+sum);
+    }
 }
