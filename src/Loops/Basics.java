09a00a2651bb88dab98cc7baa569889179aa4c19
itsirajul <shirajulislamparvez@gmail.com>
2023-02-10 22:22:20 +0600

    Maintaining Streak

--- src/Loops/Basics.java
+++ src/Loops/Basics.java
@@ -4,6 +4,7 @@ public class Basics {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Same");
+        System.out.println("Hello");
 
         //While Loop
         //For Loop
