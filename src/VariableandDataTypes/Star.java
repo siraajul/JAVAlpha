0229934ec74ad69d2b5a609fcf5fc0131d284721
siraajul <shirajulislamparvez@gmail.com>
2023-08-26 09:06:23 +0600

    Star Pattern

---
+++ src/VariableandDataTypes/Star.java
@@ -0,0 +1,10 @@
+package VariableandDataTypes;
+
+public class Star {
+    public static void main(String[] args){
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+    }
+}
